64c672f1707d5b6ecf08212ba0a683269457df23
Peter Schaeffer <peter.schaeffer@wassa.fr>
2014-12-17 10:24:40 +0100

    Initial commit

---
+++ Example/CaptchaFace/CaptchaFaceViewController.h
@@ -0,0 +1,13 @@
+//
+//  CaptchaFaceViewController.h
+//  CaptchaFace
+//
+//  Created by Peter Schaeffer on 12/17/2014.
+//  Copyright (c) 2014 Peter Schaeffer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CaptchaFaceViewController : UIViewController
+
+@end
---
+++ Example/CaptchaFace/CaptchaFaceViewController.m
@@ -0,0 +1,29 @@
+//
+//  CaptchaFaceViewController.m
+//  CaptchaFace
+//
+//  Created by Peter Schaeffer on 12/17/2014.
+//  Copyright (c) 2014 Peter Schaeffer. All rights reserved.
+//
+
+#import "CaptchaFaceViewController.h"
+
+@interface CaptchaFaceViewController ()
+
+@end
+
+@implementation CaptchaFaceViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
